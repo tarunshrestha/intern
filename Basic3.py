f6e13543aad4008c5bcd83875ade4e3e35a0f0a0
tarunshrestha <128881726+tarunshrestha@users.noreply.github.com>
2024-04-24 12:19:26 +0545

    Keywords

--- Basic3.py
+++ Basic3.py
@@ -1,37 +1,59 @@
-import requests
-import json
+# import requests
+# import json
 
-url = 'https://raw.githubusercontent.com/awasekhirni/jsondata/master/100books.json'
-response = requests.get(url)
-data = response.json()
-# print(data)
+# url = 'https://raw.githubusercontent.com/awasekhirni/jsondata/master/100books.json'
+# response = requests.get(url)
+# data = response.json()
+# # print(data)
 
-countries = list({i['country'] for i in data})
-# print(countries)
-# print(len(countries))
+# countries = list({i['country'] for i in data})
+# # print(countries)
+# # print(len(countries))
 
-newDict = {}
-# To add in new dictionary
-for i in data:
-    for j in countries:
-        if j == i['country']:
-            newDict.setdefault(j, []).append(i)
+# newDict = {}
+# # To add in new dictionary
+# for i in data:
+#     for j in countries:
+#         if j == i['country']:
+#             newDict.setdefault(j, []).append(i)
             
-# To remove country from new dict
-for i in newDict:
-    for j in newDict[i]:
-            j.pop('country')
+# # To remove country from new dict
+# for i in newDict:
+#     for j in newDict[i]:
+#             j.pop('country')
 
-print(newDict)
+# print(newDict)
 
-json_object = json.dumps(newDict, indent=2)
+# # # ______------____----Delete Dictionary
+# # a = {1:10, 2:20, 3:30}
+# # a.pop(1)
+# # print(a)
 
+# myLis = [1, 2, 3, 45, 6]
+# print(id(myLis))
+# a = myLis
+# b = myLis.copy()
 
-with open("data2.json", "w") as file:
-    file.write(json_object)
+# print(id(a))
+# print(id(b))
 
+# # del a[1]
+# # del b[1]
 
-# # ______------____----Delete Dictionary
-# a = {1:10, 2:20, 3:30}
-# a.pop(1)
+# print(myLis)
 # print(a)
+# print(b)
+
+# print(myLis == a)
+# print(myLis == b)
+# print(myLis is a)
+# print(myLis is b)
+
+def tryi():
+    for i in range(0, 5):
+        yield i
+    
+print(next(tryi()))
+
+for i in tryi():
+    print(i)
