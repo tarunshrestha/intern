97a237b05148af39a29c6746fc3eb8040799d354
tarunshrestha <tarunshrestha90@gmail.com>
2024-05-06 14:35:39 +0545

    "ORM learning School"

--- Django/CRUD/crud/base/views.py
+++ Django/CRUD/crud/base/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Checking.......")
+
+def login_page(request):
+    
+    return HttpResponse("Login-Page")
