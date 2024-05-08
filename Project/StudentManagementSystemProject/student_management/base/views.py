59f622e594a5151c1e0877df6901dcc715dcb486
tarunshrestha <tarunshrestha90@gmail.com>
2024-05-08 17:41:47 +0545

    Registration Complete

--- Project/StudentManagementSystemProject/student_management/base/views.py
+++ Project/StudentManagementSystemProject/student_management/base/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
 from django.shortcuts import get_object_or_404
 from .forms import *
+from django.contrib import messages
+
 
 # Create your views here.
 def index(request):
@@ -24,14 +26,57 @@ def register_user(request):
         last_name = request.POST['lastname']
         username = request.POST['username']
         birth_date = request.POST['birth_date']
+        address = request.POST['address'] 
         phone_number = request.POST['phone_number']
         user_type = request.POST['user_type']
         password = request.POST['password']
         password2 = request.POST['password2']
         faculty = request.POST['faculty']
-        
-        print("-----------------------Tarun-----------------------------",faculty)
-        return HttpResponse("Chalyo")
+
+        if first_name == '' or last_name == '' or address == '' or username == '' or phone_number == '' or password == '':
+            messages.error(request,'All Fields should be filled.')
+            return  redirect('register')
+
+        if password != password2:
+            messages.error(request,'Password doesnot match with Password2.')
+            return  redirect('register')
+        else:
+            if len(password) < 8:
+                messages.error(request,'Password should contain special charecters and have 8 letters.')
+                return  redirect('register')
+    
+        try : 
+            if (Teacher.objects.get(username = username) != None) or (Student.objects.get(username = username) != None) :
+                pass
+        except:
+            pass
+        else:
+            messages.error(request,'Username already exists.')
+            return  redirect('register')
+
+        if user_type == "Teacher":
+            user = Teacher.objects.create(first_name=first_name,
+                                           last_name=last_name, 
+                                           username=username, 
+                                           password=password, 
+                                           birth_date=birth_date, 
+                                           address=address,
+                                           phone_number=phone_number, 
+                                           faculty=Faculty.objects.get(name=faculty))
+        else:
+            user = Student.objects.create(first_name=first_name,
+                                           last_name=last_name, 
+                                           username=username, 
+                                           password=password, 
+                                           birth_date=birth_date, 
+                                           address=address,
+                                           phone_number=phone_number, 
+                                           faculty=Faculty.objects.get(name=faculty))
+        user.save()
+        # user = LoginForm(username=username, password=password)
+        # LoginForm(request, user)
+        # messages.success(request, 'You are now registered')
+        return redirect("home")
     else:
         form = RegisterForm()
         return render(request, "register.html", context={'form':form, 'faculty':faculty})
