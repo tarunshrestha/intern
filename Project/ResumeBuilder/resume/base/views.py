bb2fa8c2426a52a8ae58542d47fe8efecf349d96
tarunshrestha <tarunshrestha90@gmail.com>
2024-05-13 10:09:52 +0545

    "FirstMigration"

--- Project/ResumeBuilder/resume/base/views.py
+++ Project/ResumeBuilder/resume/base/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from .forms import *
+from django.contrib import messages
 
 
 # Create your views here.
@@ -11,7 +12,14 @@ def index(request):
 
 def register_user(request):
     if request.method == "POST":
-        form = SignUpForm(request.Post)
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect ('home')
+        else:
+             messages.error(request, "Please check the form again.")
+             return redirect('register')
+             
     else:
         form = SignUpForm()
         return render(request, "register.html")
