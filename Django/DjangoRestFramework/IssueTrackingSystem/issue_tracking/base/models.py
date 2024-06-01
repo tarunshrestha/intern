e824e581fe5585d293aed1c1a17ab7b83ff7241f
tarunshrestha <tarunshrestha90@gmail.com>
2024-06-01 20:35:36 +0545

    serializer

--- Django/DjangoRestFramework/IssueTrackingSystem/issue_tracking/base/models.py
+++ Django/DjangoRestFramework/IssueTrackingSystem/issue_tracking/base/models.py
@@ -28,21 +28,6 @@ class CustomUser(AbstractUser):
     def __str__(self):
        return self.username
 
-    groups = models.ManyToManyField(
-        Group,
-        related_name='customuser_set',  
-        blank=True,
-        help_text=('The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
-        related_query_name='customuser'
-    )
-    user_permissions = models.ManyToManyField(
-        Permission,
-        related_name='customuser_set',  
-        blank=True,
-        help_text=('Specific permissions for this user.'),
-        related_query_name='customuser'
-    )
-
 class Company(models.Model):
     name = models.CharField(max_length=150, unique=True)
     address = models.CharField(max_length=150)
---
+++ Django/DjangoRestFramework/IssueTrackingSystem/issue_tracking/base/serializer.py
@@ -0,0 +1,74 @@
+from rest_framework import serializers
+from django.template.defaultfilters import slugify
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from django.contrib.auth.hashers import make_password  
+from .models import *
+import re
+from rest_framework.validators import UniqueTogetherValidator
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(required=True, write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'password', 'password2', 'date_joined', 'first_name', 'last_name', 'username', 'groups', 'phone', 'date_of_birth', 'profile_picture', 'gender', 'email', 'is_verified', 'otp']
+
+    def validate(self, data):
+        password = data['password']
+        print(data)
+        if  data['password'] != data['password2']:
+            raise serializers.ValidationError({'password':'Passwords doesnot match.'})
+        if len(password) < 8 :
+            raise serializers.ValidationError({'password': 'Password must have more then 8 charecters.'})
+        print(data)
+        return data
+    
+    def create(self, validated_data):
+        validated_data.pop('password2', None)
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data.pop('password2', None)
+        return super().update(instance, validated_data)
+    
+
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        email = data.get('email')
+        password = data.get('password')
+        if not CustomUser.objects.filter(email = email).exists():
+            raise serializers.ValidationError({'email':'Email doesnot exists.'})
+        user = CustomUser.objects.get(email = email)
+        if user.password != password:
+            raise serializers.ValidationError({'password':'Password not matched.'})
+        if email and password:
+            user = authenticate(email=email, password=password)
+        data['email'] = email.lower()
+        data['user'] = user
+        return data
+    
+
+class TicketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = '__all__'
+
+
+    def validate(self, validated_data):
+        return validated_data
+    
+    def create(self, validated_data):
+        return super().create(validated_data)
+    
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
+
+
+
