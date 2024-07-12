241e5f66c17b2dcc00ecb73d73a057a2e9576cd2
tarun <tarun.shrestha@aayulogic.com>
2024-07-12 11:33:24 +0545

    datafeeder

--- Project/CornJobPractice/cron_project/base/views.py
+++ Project/CornJobPractice/cron_project/base/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Test
 
 # Create your views here.
+def autofeed(request): 
+    data = ["Test", "Choice", "me"]   
+    try:
+        for i in data:
+            instance = Test.objects.get_or_create(name=i)
+        return HttpResponse("DONE")
+    except Exception as e :
+        return HttpResponse(f'Error: {e}')
+    
