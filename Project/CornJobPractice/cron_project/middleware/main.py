d3226984a5e6d89fa35f0e6bf71190463ff00c40
tarun <tarun.shrestha@aayulogic.com>
2024-07-15 14:31:45 +0545

    middleware try

---
+++ Project/CornJobPractice/cron_project/middleware/main.py
@@ -0,0 +1,7 @@
+class ExampleMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request, *args, **kwargs):
+        response = self.get_response
+        
